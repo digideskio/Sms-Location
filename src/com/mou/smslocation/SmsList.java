2532cc74b22b7bcadc5caa1cbef55b4e4993a13c
alies_a <arnaud.alies@epitech.eu>
2016-03-12 20:00:23 +0100

    base

---
+++ src/com/mou/smslocation/SmsList.java
@@ -0,0 +1,63 @@
+package com.mou.smslocation;
+
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class SmsList extends Activity {
+	private String TAG = "SmsLocationMain";
+	private ListView smsList;
+	private Context context;
+	
+	private String[] getSmsArray()
+	{
+		String res[];
+		StringBuffer buff = new StringBuffer();
+		SQLiteDatabase db;
+		Cursor cur;
+		
+		db = context.openOrCreateDatabase(context.getString(R.string.db_name), Context.MODE_PRIVATE, null);
+		db.execSQL("CREATE TABLE IF NOT EXISTS sms(number VARCHAR, data VARCHAR);");
+		cur = db.rawQuery("SELECT * FROM sms;", null);
+		while (cur.moveToNext())
+		{
+			buff.append(cur.getString(0) + ":"+ cur.getString(1));
+			buff.append("\n");
+		}
+		db.close();
+		res = buff.toString().split("\n");
+		return (res);
+	}
+	private void reloadData()
+	{
+		String[] res;
+		
+		res = getSmsArray();
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+				getApplicationContext(),
+				android.R.layout.simple_list_item_1,
+				res);
+		smsList.setAdapter(adapter);
+	}
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sms_list);
+		
+		context = getApplicationContext();
+		smsList = (ListView) findViewById(R.id.smslist);
+		
+		reloadData();
+	}
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+		reloadData();
+	}
+}
