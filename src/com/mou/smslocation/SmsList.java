9d71f1b03d6bef17785f5e18e7304d370ab16fba
alies_a <arnaud.alies@epitech.eu>
2016-03-12 21:39:19 +0100

    select each data of the database

--- src/com/mou/smslocation/SmsList.java
+++ src/com/mou/smslocation/SmsList.java
@@ -11,13 +11,14 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class SmsList extends Activity {
 	private String TAG = "SmsLocationMain";
 	private ListView smsList;
 	private Context context;
 	
-	public String[] getSmsArray()
+	public String[] getSmsArray(int select)
 	{
 		String res[];
 		StringBuffer buff = new StringBuffer();
@@ -29,7 +30,8 @@ public class SmsList extends Activity {
 		cur = db.rawQuery("SELECT * FROM sms ORDER BY id DESC;", null);
 		while (cur.moveToNext())
 		{
-			buff.append("id: " + cur.getString(2) + "/" + cur.getString(0) + ":"+ cur.getString(1));
+			//buff.append("id: " + cur.getString(2) + "/" + cur.getString(0) + ":"+ cur.getString(1));
+			buff.append(cur.getString(select));
 			buff.append("\n");
 		}
 		db.close();
@@ -40,7 +42,7 @@ public class SmsList extends Activity {
 	{
 		String[] res;
 		
-		res = getSmsArray();
+		res = getSmsArray(0);
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
 				getApplicationContext(),
 				android.R.layout.simple_list_item_1,
@@ -58,7 +60,10 @@ public class SmsList extends Activity {
 		smsList.setOnItemClickListener(new OnItemClickListener(){
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				String[] coords;
 				
+				coords = getSmsArray(1);
+				Toast.makeText(context, coords[(int)id], Toast.LENGTH_SHORT).show();
 			}
 		});
 		
